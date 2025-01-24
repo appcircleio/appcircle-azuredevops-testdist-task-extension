d5da1d3986d1a58dbd5d15c803f4952f04a75b58
Onur Yılmaz <csonuryilmaz@gmail.com>
2025-01-24 17:29:49 +0300

    BE-5437 Add Jenkins pipeline for plugin CI/CD (#7)

---
+++ pipeline.sh
@@ -0,0 +1,54 @@
+#!/bin/env bash
+
+# shellcheck shell=bash
+set -euo pipefail
+
+echo "Publish Token: $(echo "$PUBLISH_TOKEN" | cut -c1-3)...***"
+
+echo "=== Runtime Dependencies ==="
+echo
+echo "npm: $(npm -v)"
+echo "node: $(node -v)"
+echo "yarn: $(yarn -v)"
+if command -v tsc &>/dev/null; then
+    echo "tsc: OK"
+    npm ls -g | grep typescript
+fi
+if command -v tfx &>/dev/null; then
+    echo "tfx: OK"
+    npm ls -g | grep tfx-cli
+fi
+echo
+
+echo "=== Update Dependencies ==="
+echo
+yarn install
+cd buildandreleasetask/
+yarn install
+cd ..
+echo
+
+echo "=== Build Package ==="
+echo
+yarn package
+
+echo "=== Create Extension ==="
+echo
+if [ "$BRANCH_NAME" == "main" ]; then
+    configuration="configs/release.json"
+elif [[ "$BRANCH_NAME" =~ ^release.* ]]; then
+    configuration="configs/dev.json"
+else
+    echo "Branch $BRANCH_NAME is not configured for release."
+    exit 1
+fi
+echo "Configuration: $configuration"
+tfx extension create --manifest-globs vss-extension.json --overrides-file $configuration
+echo
+
+echo "=== Publish Extension ==="
+echo
+tfx extension publish --manifest-globs vss-extension.json --overrides-file $configuration --token "$PUBLISH_TOKEN"
+echo
+
+exit 0
